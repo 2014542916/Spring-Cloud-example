8a3f932774b5362cf3107e12e71a657384c10ee1
lihaojie <2014542916@qq.com>
2023-03-07 11:20:30 +0800

    aaa

--- lagou-service-resume/src/main/java/com/lhj/learn/dao/ResumeDao.java
+++
@@ -1,8 +0,0 @@
-package com.lhj.learn.dao;
-
-
-import com.lhj.learn.pojo.Resume;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResumeDao extends JpaRepository<Resume,Long> {
-}
